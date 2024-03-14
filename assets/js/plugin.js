6a36c8b40f89a1875948f8820b252bbaf64bc0b4
Fathi Mohammed <fathymohammed2229@gmail.com>
2024-03-14 16:03:14 +0200

    last changes

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function ($) {
   uploadProfilePhoto();
   chatScrollDirectionDown();
   verificationTimer();
-  stopFancyboxOnClickOnDeleteButton();
+  // stopFancyboxOnClickOnDeleteButton();
 });
 
 // functions init
